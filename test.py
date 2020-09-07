a4c01d3f645ce0084f5f031fd34f560a62e82074
actnlp <actnlp@163.com>
2020-09-07 18:56:27 +0800

    test.py

--- test.py
+++ test.py
@@ -18,28 +18,29 @@ def test_qa(fname, use_full=True):  # use_full ???  包括拒识？
     #     fname = os.path.join(TEST_FILE_DIR, fname)
     # with open(fname, 'r', encoding='utf-8') as f:
     #     quesitons = [line.strip() for line in f][0:1]
-    quesitons = ['航线：']
+    quesitons = ["中国东方航空的简称是什么"]
     if use_full:
         qa = QAFull()
     else:
         qa = QA()
     ans = [qa.answer(q) for q in tqdm(quesitons)]
+    print(ans)
 
-    ans_text = ['\n'.join(map(lambda x:x['natural_ans'], a[:3])) for a in ans]
-    ans_df = pd.DataFrame({'question': quesitons, 'answer top3': ans_text})
-    basename = os.path.basename(fname).split('.')[0]
-    output_fname = os.path.join(TEST_RESULT_DIR, basename + '_res.xlsx')
-    # 与上一次结果对比
-    if os.path.isfile(output_fname):
-        old_df = pd.read_excel(output_fname)
-        if 'answer top3' in old_df:
-            idx = old_df['answer top3'] != ans_df['answer top3']
-            diff_df = pd.merge(old_df[idx], ans_df[idx],
-                               left_index=True, right_index=True, how='left')
-            diff_fname = os.path.join(TEST_RESULT_DIR, basename + '_diff.xlsx')
-            diff_df.to_excel(diff_fname)
+    # ans_text = ['\n'.join(map(lambda x:x['natural_ans'], a[:3])) for a in ans]
+    # ans_df = pd.DataFrame({'question': quesitons, 'answer top3': ans_text})
+    # basename = os.path.basename(fname).split('.')[0]
+    # output_fname = os.path.join(TEST_RESULT_DIR, basename + '_res.xlsx')
+    # # 与上一次结果对比
+    # if os.path.isfile(output_fname):
+    #     old_df = pd.read_excel(output_fname)
+    #     if 'answer top3' in old_df:
+    #         idx = old_df['answer top3'] != ans_df['answer top3']
+    #         diff_df = pd.merge(old_df[idx], ans_df[idx],
+    #                            left_index=True, right_index=True, how='left')
+    #         diff_fname = os.path.join(TEST_RESULT_DIR, basename + '_diff.xlsx')
+    #         diff_df.to_excel(diff_fname)
 
-    ans_df.to_excel(output_fname)
+    # ans_df.to_excel(output_fname)
 
 
 if __name__ == "__main__":
